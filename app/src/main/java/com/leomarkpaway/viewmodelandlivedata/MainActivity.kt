aaff5465429f0d05d6924ec6bb72b6ac61c1881a
leomark paway <leomarkpaway@gmail.com>
2024-07-02 13:02:57 +0800

    :sparkles: Create buttons

--- app/src/main/java/com/leomarkpaway/viewmodelandlivedata/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/viewmodelandlivedata/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.leomarkpaway.viewmodelandlivedata
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.viewmodel.ViewModelInitializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val incrementButton = findViewById<Button>(R.id.btn_increment)
+        val resetButton = findViewById<Button>(R.id.btn_reset)
+        val decrementButton = findViewById<Button>(R.id.btn_decrement)
     }
 }

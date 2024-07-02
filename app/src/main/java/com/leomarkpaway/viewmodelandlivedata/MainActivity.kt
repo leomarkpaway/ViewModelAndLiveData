4ce098f176b464402106886e8589d0821ccbe36a
leomark paway <leomarkpaway@gmail.com>
2024-07-02 13:45:43 +0800

    :sparkles: Implement counter functions

--- app/src/main/java/com/leomarkpaway/viewmodelandlivedata/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/viewmodelandlivedata/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.leomarkpaway.viewmodelandlivedata
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.viewmodel.ViewModelInitializer
-import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,8 +26,19 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val counterText = findViewById<TextView>(R.id.tv_counter)
         val incrementButton = findViewById<Button>(R.id.btn_increment)
         val resetButton = findViewById<Button>(R.id.btn_reset)
         val decrementButton = findViewById<Button>(R.id.btn_decrement)
+
+        incrementButton.setOnClickListener { viewModel.increment() }
+        resetButton.setOnClickListener { viewModel.reset() }
+        decrementButton.setOnClickListener { viewModel.decrement() }
+
+        viewModel.counter.observe(this) { value ->
+            counterText.text = value.toString()
+            Log.d("qwe", "$value")
+        }
+
     }
 }
